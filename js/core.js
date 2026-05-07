de64156333d58e705289fa822771611677080f3a
olly-techie <ollyverse01@gmail.com>
2026-05-07 23:54:36 +0100

    updates

--- js/core.js
+++ js/core.js
@@ -165,7 +165,7 @@ AqiNode.register('footer', () => `
             <img src="img/logo.png" alt="AqiNode">
             <span><em>Aqi</em>Node</span>
           </a>
-          <p>Building intelligent, scalable digital systems that solve real-world problems and grow with your business.</p>
+          <p>Building intelligent, scalable digital systems that solve real-world problems.</p>
           <div class="footer-socials">
             <a href="https://x.com/aqinode_hq" target="_blank" rel="noopener" class="social-btn" title="X / Twitter">
               <svg width="15" height="15" viewBox="0 0 24 24" fill="currentColor"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.744l7.73-8.835L1.254 2.25H8.08l4.253 5.622zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>
